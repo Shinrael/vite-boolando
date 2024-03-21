b84471cfa126cb5b7b1aced6c6db4f0ce234564e
Shinrael <mircoelena89@gmail.com>
2024-03-21 18:01:14 +0100

    [add] menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,35 @@
 <script>
   export default {
-    
+    data(){
+      return{
+        menu:[
+          {
+            title: 'Donna',
+            link: '#',
+          },
+          {
+            title: 'Uomo',
+            link: '#',
+          },
+          {
+            title: 'Bambino',
+            link: '#',
+          },
+        ]
+      }
+    }
   }
 </script>
 
 <template>
   <header class="d-flex justify-content-around align-items-center">
-    <div class="menu d-flex align-items-center justify-content-center">AAA</div>
+    <div class="menu d-flex justify-content-center">
+      <ul class="d-flex list-unstyled align-items-center">
+        <li v-for="(item,index) in menu" :key="index">
+          <a href="{{ item.link }}">{{ item.title }}</a>
+        </li>        
+      </ul>
+    </div>
     <div class="logo d-flex align-items-center justify-content-center">
       <div class="img-container">
         <img src="/public/boolean-logo.png" alt="logo">
@@ -27,6 +50,17 @@ header{
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
   .menu{
     width: 33%;
+    ul{
+      li{
+        padding: 0px 10px;        
+        font-weight: bold;
+        cursor: pointer;
+        a{
+          color: $color-header;
+          text-decoration: none;
+        }
+      }
+    }
   }
   .logo{
     width: 33%;
