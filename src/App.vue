daa21cef85c8f87391b3a1f5cccd620bb59cbe46
Shinrael <mircoelena89@gmail.com>
2024-03-21 15:43:52 +0100

    [add] header, main, footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
   export default {
-    
+    components:{
+      Header,
+      Main, 
+      Footer
+    }
   }
 </script>
 
 <template>
-  <div>
-    Prova
-  </div>
+  
+  <Header />
+  <Main />
+  <Footer />
+  
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+@use './assets/scss/main.scss';
 
 </style>
